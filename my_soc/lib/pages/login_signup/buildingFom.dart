fb864f34d3d4de209912a713a6e366fd1adc686e
Vedant Rajendra Shetye <vedantblackops@gmail.com>
2025-02-12 23:37:28 +0530

    Remove unused import for 'dart:io' in buildingFom.dart

--- my_soc/lib/pages/login_signup/buildingFom.dart
+++ my_soc/lib/pages/login_signup/buildingFom.dart
@@ -12,7 +12,6 @@ import 'package:my_soc/pages/login_signup/chooseMap.dart';
 import 'package:my_soc/routes.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:convert';
-import 'dart:io';
 import 'package:flutter/services.dart';
 
 class WingDetails {
